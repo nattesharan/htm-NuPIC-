baf2adf874b09eea62545c6d5989e6a7aff81eb6
Natte <nattesharan@gmail.com>
2017-05-13 00:45:40 +0530

    Added script for creating another directory which stores  model params

--- swarm.py
+++ swarm.py
@@ -1,11 +1,22 @@
 from nupic.swarming import permutations_runner
 from swarm_description import SWARM_DESCRIPTION
 import os
+import pprint
+def writeModelParams(modelParams):
+    outDir = os.path.join(os.getcwd(), "model_params")
+    if not os.path.isdir(outDir):
+        os.mkdir(outDir)
+    outPath = os.path.join(outDir, "model_params.py")
+    pp = pprint.PrettyPrinter(indent=2)
+    with open(outPath, "wb") as outFile:
+        modelParamsString = pp.pformat(modelParams)
+        outFile.write("MODEL_PARAMS = \\\n%s" %modelParamsString)
+    return outPath
 def swarm(inputFile):
     swarmWorkDir = os.path.abspath("swarm")
     if not os.path.exists(swarmWorkDir):
         os.mkdir(swarmWorkDir)
-    permutations_runner.runWithConfig(
+    modelParams = permutations_runner.runWithConfig(
         SWARM_DESCRIPTION,
         {
             "maxWorkers": 4,
@@ -15,5 +26,6 @@ def swarm(inputFile):
         outDir =swarmWorkDir,
         permWorkDir =swarmWorkDir
     )
+    writeModelParams(modelParams)
 if __name__ == '__main__':
     swarm('rec-center-hourly.csv')
