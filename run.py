49f6755e97d7122e45c20a46609a970c7bd846bf
Natte <nattesharan@gmail.com>
2017-05-13 01:52:50 +0530

    It predicts the values

---
+++ run.py
@@ -0,0 +1,39 @@
+from nupic.frameworks.opf.modelfactory import ModelFactory
+from model_params import model_params
+import datetime
+import csv
+DATE_FORMAT = "%m/%d/%y %H:%M"
+def createModel():
+    print("executing")
+    model = ModelFactory.create(model_params.MODEL_PARAMS)
+    model.enableInference({
+        "predictedField": "kw_energy_consumption"
+    })
+    return model
+def runModel(model):
+    print("executing")
+    inputFilePath = "rec-center-hourly.csv"
+    inputFile = open(inputFilePath, "rb")
+    csvReader = csv.reader(inputFile)
+    csvReader.next()
+    csvReader.next()
+    csvReader.next()
+    counter = 0
+    for row in csvReader:
+        counter += 1
+        if counter % 100 == 0:
+            print("Read %i lines ..."%counter)
+        timestamp = datetime.datetime.strptime(row[0], DATE_FORMAT)
+        consumption = row[1]
+        result = model.run({
+            "timestamp": timestamp,
+            "kw_energy_consumption": consumption
+        })
+        prediction = result.inferences["multiStepBestPredictions"][1]
+        print(prediction)
+def runHotGym():
+    print("executing")
+    model = createModel()
+    runModel(model)
+if __name__ == "__main__":   
+    runHotGym()
