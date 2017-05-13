4ffc010bbb1f73e944158a5051248c3fdc9341fc
Natte <nattesharan@gmail.com>
2017-05-13 12:04:28 +0530

    Now the predictions are stored in a csv

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from nupic.frameworks.opf.modelfactory import ModelFactory
 from model_params import model_params
+from nupic.data.inference_shifter import InferenceShifter
+import nupic_output
 import datetime
 import csv
 DATE_FORMAT = "%m/%d/%y %H:%M"
@@ -18,19 +20,24 @@ def runModel(model):
     csvReader.next()
     csvReader.next()
     csvReader.next()
+    shifter = InferenceShifter()
+    output = nupic_output.NuPICPlotOutput(["Rec Center"])
     counter = 0
     for row in csvReader:
         counter += 1
         if counter % 100 == 0:
             print("Read %i lines ..."%counter)
         timestamp = datetime.datetime.strptime(row[0], DATE_FORMAT)
-        consumption = row[1]
+        consumption = float(row[1])
         result = model.run({
             "timestamp": timestamp,
             "kw_energy_consumption": consumption
         })
+        result = shifter.shift(result)
         prediction = result.inferences["multiStepBestPredictions"][1]
-        print(prediction)
+        output.write([timestamp], [consumption], [prediction])
+    inputFile.close()
+    output.close()
 def runHotGym():
     print("executing")
     model = createModel()
